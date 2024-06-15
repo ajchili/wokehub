adb6ea6f26c678ffe5946860633d61758476b4da
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-06-15 13:32:47 -0500

    🧹 Clean

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import { Header } from "../components/Header/Header";
 import { Search } from "../components/Search/Search";
-import { UserInfo } from "../components/UserInfo/UserInfo";
 import { ClientProviders } from "./providers";
 
 export default function Home() {
-  const username = "hacksore";
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div
