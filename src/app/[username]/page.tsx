a58cd7dfa9c4b0fff72fe62065b2f67c80f964dd
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-06-15 14:07:50 -0500

    🪣 Add buckets of repos

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -1,6 +1,44 @@
 import { Repos } from "./repos";
 import { Octokit } from "octokit";
 
+interface RepoListReponse {
+  user: {
+    login: string;
+    repositories: {
+      nodes: {
+        url: string;
+        name: string;
+        updatedAt: string;
+        createdAt: string;
+        defaultBranchRef: {
+          name: string;
+        };
+      }[];
+      pageInfo: {
+        hasNextPage: boolean;
+        endCursor: string;
+      };
+    };
+  };
+}
+
+function reduceRepoPages(input: RepoListReponse): Record<string, string[]> {
+  const data: Record<string, string[]> = {};
+
+  for (const repo of input.user.repositories.nodes) {
+    const { name, defaultBranchRef } = repo;
+    const branchName = defaultBranchRef?.name;
+
+    if (!data[branchName]) {
+      data[branchName] = [];
+    }
+
+    data[branchName].push(name);
+  }
+
+  return data;
+}
+
 export default async function Home({
   params: { username },
 }: {
@@ -11,7 +49,7 @@ export default async function Home({
   });
 
   // NOTE: one thing that sucks about this is it not cacheable easily
-  const repoInfo = await octokit.graphql.paginate(
+  const repoInfo = (await octokit.graphql.paginate(
     `query GetUsernameAndRepos($username: String!, $num: Int = 100, $cursor: String) {
         user(login: $username) {
           login,
@@ -35,7 +73,10 @@ export default async function Home({
     {
       username,
     },
-  );
+  )) as RepoListReponse;
 
-  return <Repos username={username} data={repoInfo} />;
+  // convert back to the old data shape?
+  const newRepoInfo = reduceRepoPages(repoInfo);
+
+  return <Repos username={username} data={newRepoInfo} />;
 }
