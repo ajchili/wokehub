4e9d8fae44dc634a63b2fb633b41d836c7c11f2a
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-06-15 21:07:22 -0500

    🖼️ Add og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,26 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "🤝 Wokehub",
-  description: "Search for a GitHub user and see their woke status",
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const title = "🤝 Wokehub";
+  const description = "Search Who is Woke on Github!";
+  return {
+    title,
+    description,
+    openGraph: {
+      images: ["https://wokehub.lol/og.png"],
+      description,
+      title,
+      type: "website",
+    },
+    twitter: {
+      images: ["https://wokehub.lol/og.png"],
+      title,
+      description,
+      card: "summary_large_image",
+    },
+  };
+}
 
 export default function RootLayout({
   children,
